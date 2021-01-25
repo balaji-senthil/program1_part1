900cc8dfc2b706db890e3f41dba2b50ec82bf513
Balaji Senthilkumar <44097181+balaji-senthil@users.noreply.github.com>
2021-01-25 16:30:55 -0700

    Create part1.py

---
+++ part1.py
@@ -0,0 +1,22 @@
+# print('Checking file output...')
+def loadBoard(board):
+    inputFile = open(board, 'r')
+    myBoard = []
+    for row in inputFile:
+        myBoard.append(row.split())
+    return myBoard
+
+
+def printBoard(myBoard):
+    for row in myBoard:
+        print (" ".join(map(str,row)))
+
+
+# def possibleMoves(currentPosition,myBoard):
+#     for 
+
+#testing output
+myBoard=loadBoard('board.txt')
+printBoard(myBoard)
+pos=(0,0)
+
