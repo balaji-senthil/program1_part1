dea216c31e663a422dd594f05b39710920704137
balaji <sbalajiadolf@gmail.com>
2021-01-28 23:42:26 -0700

    possibleMoves done

--- part1.py
+++ part1.py
@@ -14,28 +14,35 @@ def printBoard(myBoard):
    
     
 def possibleMoves(currentPosition,myBoard):
-    r=int(currentPosition[0])
-    c=int(currentPosition[1])
-    x=r
-    y=c
+    x=int(currentPosition[0])
+    y=int(currentPosition[1])
     possibleMovesArray=[]
-    #print(myBoard[r][c]) - checking unpacking of the set to the myBoard
-    # print(len(myBoard))
-    while(1):
-        if(x+1<len(myBoard), y+1<len(myBoard)):
-            possibleMovesArray.append((x+1,y+1))
-        break
-    print(possibleMovesArray)
-
-
+    limit=len(myBoard)
+    if(x+1<limit and y<limit):#left to right
+        possibleMovesArray.append((x+1,y))
+    if(x<limit and y+1<limit):#Top down
+        possibleMovesArray.append((x,y+1))
+    if(x+1<limit and y+1<limit):#diagonal 1
+        possibleMovesArray.append((x+1,y+1))
+    if(x+1<limit and ((y-1<limit)and y-1>=0)):#diagonal 2
+        possibleMovesArray.append((x+1,y-1))
+    if(((x-1<limit)and x-1>=0) and ((y-1<limit)and y-1>=0)):#diagonal 3
+        possibleMovesArray.append((x-1,y-1))
+    if(((x-1<limit)and x-1>=0) and (y+1<limit)):#diagonal 4
+        possibleMovesArray.append((x-1,y+1))
+    if(((x-1<limit)and x-1>=0) and y<limit):#right to left
+        possibleMovesArray.append((x-1,y))
+    if(x<limit and ((y-1<limit)and y-1>=0)):#Bottom Up
+        possibleMovesArray.append((x,y-1))
+    return possibleMovesArray
 
     
-
-    
-
 #testing output of part1
 myBoard=loadBoard('board.txt')
 printBoard(myBoard)
-pos=(0,0)#get user input
-possibleMoves(pos,myBoard)
+pos=(2,2)#get user input
+print(possibleMoves(pos,myBoard))
+
+
+
 
