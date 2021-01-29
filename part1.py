7eeebe13989c54a3e9b110dfd6fde093126a46ae
balaji <sbalajiadolf@gmail.com>
2021-01-28 17:26:13 -0700

    possibleMoves added

--- part1.py
+++ part1.py
@@ -10,13 +10,32 @@ def loadBoard(board):
 def printBoard(myBoard):
     for row in myBoard:
         print (" ".join(map(str,row)))
+    # print('****'+myBoard[2][3])
+   
+    
+def possibleMoves(currentPosition,myBoard):
+    r=int(currentPosition[0])
+    c=int(currentPosition[1])
+    x=r
+    y=c
+    possibleMovesArray=[]
+    #print(myBoard[r][c]) - checking unpacking of the set to the myBoard
+    # print(len(myBoard))
+    while(1):
+        if(x+1<len(myBoard), y+1<len(myBoard)):
+            possibleMovesArray.append((x+1,y+1))
+        break
+    print(possibleMovesArray)
 
 
-# def possibleMoves(currentPosition,myBoard):
-#     for i 
+
+    
+
+    
 
 #testing output
 myBoard=loadBoard('board.txt')
 printBoard(myBoard)
-pos=(0,0)
+pos=(0,0)#get user input
+possibleMoves(pos,myBoard)
 
